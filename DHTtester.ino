42e98a6aed86151e40abd13f107237a5e3de76e8
Rajat Srivastava <166909708+Rajat-Srivastava-19@users.noreply.github.com>
2025-04-03 15:42:17 +0530

    Add DHT11 integration test with Arduino Uno

---
+++ DHTtester.ino
@@ -0,0 +1,38 @@
+
+#include "DHT.h"
+
+#define DHTPIN 2     
+
+
+#define DHTTYPE DHT11   
+
+DHT dht(DHTPIN, DHTTYPE);
+
+void setup() {
+  Serial.begin(9600);
+  Serial.println(F("DHTxx test!"));
+
+  dht.begin();
+}
+
+void loop() {
+  // Wait a few seconds between measurements.
+ 
+
+ 
+  float h = dht.readHumidity();
+  // Read temperature as Celsius (the default)
+  float t = dht.readTemperature();
+  // Read temperature as Fahrenheit (isFahrenheit = true)
+  float f = dht.readTemperature(true);
+ 
+
+
+  Serial.print(F("Humidity: "));
+  Serial.print(h);
+  Serial.print(F("%  Temperature: "));
+  Serial.print(t);
+  Serial.println(F("°C "));
+  delay(1000);
+  
+}
